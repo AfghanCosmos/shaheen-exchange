e43d3acc7f72e38b05c87fbf978670c07b921fce
Sabawoon-Khan <sabawoonkhan582@gmail.com>
2025-03-20 12:12:01 +0430

    changes

---
+++ app/Filament/Resources/FundRequestResource.php
@@ -0,0 +1,130 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\FundRequestResource\Pages;
+use App\Filament\Resources\FundRequestResource\RelationManagers;
+use App\Models\FundRequest;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class FundRequestResource extends Resource
+{
+    protected static ?string $model = FundRequest::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('uuid')
+                    ->label('UUID')
+                    ->required()
+                    ->maxLength(36),
+                Forms\Components\TextInput::make('offline_transfer_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('store_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('amount_requested')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('currency_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('status')
+                    ->required(),
+                Forms\Components\TextInput::make('admin_id')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\DateTimePicker::make('approved_at'),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('uuid')
+                    ->label('UUID')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('offline_transfer_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('store_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('amount_requested')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('currency_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('admin_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('approved_at')
+                    ->dateTime()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListFundRequests::route('/'),
+            'create' => Pages\CreateFundRequest::route('/create'),
+            'view' => Pages\ViewFundRequest::route('/{record}'),
+            'edit' => Pages\EditFundRequest::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
---
+++ app/Filament/Resources/TransferResource.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\TransferResource\Pages;
+use App\Filament\Resources\TransferResource\RelationManagers;
+use App\Models\Transfer;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class TransferResource extends Resource
+{
+    protected static ?string $model = Transfer::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('uuid')
+                    ->label('UUID')
+                    ->required()
+                    ->maxLength(36),
+                Forms\Components\TextInput::make('sender_wallet_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('receiver_wallet_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('amount')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('fee')
+                    ->required()
+                    ->numeric()
+                    ->default(0.00),
+                Forms\Components\TextInput::make('total_amount')
+                    ->numeric()
+                    ->default(null),
+                Forms\Components\TextInput::make('currency_id')
+                    ->required()
+                    ->numeric(),
+                Forms\Components\TextInput::make('status')
+                    ->required(),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('uuid')
+                    ->label('UUID')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('sender_wallet_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('receiver_wallet_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('amount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('fee')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('total_amount')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('currency_id')
+                    ->numeric()
+                    ->sortable(),
+                Tables\Columns\TextColumn::make('status'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListTransfers::route('/'),
+            'create' => Pages\CreateTransfer::route('/create'),
+            'view' => Pages\ViewTransfer::route('/{record}'),
+            'edit' => Pages\EditTransfer::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+}
